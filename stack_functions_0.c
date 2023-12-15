7938dff5a3dda5e4ad864d4e647ec68bc1be7d3d
obafisayo-alx <sekaniisjoy@gmail.com>
2023-12-15 04:03:18 +0100

    added pall

--- stack_functions_0.c
+++ stack_functions_0.c
@@ -34,6 +34,26 @@ void push(stack_t **stack, unsigned int nline)
 }
 
 /**
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ *
+ * Return: Nothing.
+ */
+void pall(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+	(void)nline;
+
+	temp = *stack;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+/**
  * free_stack - frees all the nodes in a stack
  * @stack: pointer to the first node of the stack
  * 
